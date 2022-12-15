d4800f5aa616d0b9c99ae69118bec1a6b08fe406
Sam Badovinac <104463646+badovinac-insight@users.noreply.github.com>
2022-12-15 12:31:56 -0500

    Update SetAccessKeyReminderStripped.ps1

--- SetAccessKeyReminderStripped.ps1
+++ SetAccessKeyReminderStripped.ps1
@@ -2,12 +2,7 @@ Param($StorageAccountName, $NumDays, $ResourceGroupName)
 
 $storageAccount = Get-AzStorageAccount -ResourceGroup $ResourceGroupName -Name $StorageAccountName
 
-if ($storageAccount.KeyCreationTime.Key1 -eq $null -or $storageAccount.KeyCreationTime.Key2 -eq $null)
-{
-    Write-Host("You must regenerate both keys at least once before setting expiration policy")
-}
-else
-{
+
     Set-AzStorageAccount -ResourceGroup $ResourceGroupName -Name $StorageAccountName  -KeyExpirationPeriodInDay $NumDays
-    #Write-Host $account.StorageAccountName "access key expiration period successfully updated to" $NumDays "days"
-}
+    Write-Host $account.StorageAccountName "access key expiration period successfully updated to" $NumDays "days"
+
