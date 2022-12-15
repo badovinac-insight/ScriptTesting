445593cb6e266459fb2b487c97141ed81db123de
Sam Badovinac <104463646+badovinac-insight@users.noreply.github.com>
2022-12-15 12:35:32 -0500

    Update SetAccessKeyReminderStripped.ps1

--- SetAccessKeyReminderStripped.ps1
+++ SetAccessKeyReminderStripped.ps1
@@ -2,7 +2,6 @@ Param($StorageAccountName, $NumDays, $ResourceGroupName)
 
 $storageAccount = Get-AzStorageAccount -ResourceGroup $ResourceGroupName -Name $StorageAccountName
 
-
-    Set-AzStorageAccount -ResourceGroup $ResourceGroupName -Name $StorageAccountName  -KeyExpirationPeriodInDay $NumDays
-    Write-Host $account.StorageAccountName "access key expiration period successfully updated to" $NumDays "days"
+Set-AzStorageAccount -ResourceGroup $ResourceGroupName -Name $StorageAccountName  -KeyExpirationPeriodInDay $NumDays
+Write-Host $account.StorageAccountName "access key expiration period successfully updated to" $NumDays "days"
 
